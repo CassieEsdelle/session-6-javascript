75e4ae2b4d352d2e0657bc0c0f962ac10b888948
CassieEsdelle <cassie_esdelle@hotmail.com>
2022-08-21 18:37:51 +0100

    added function conversation

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ console.log("Hello everyone")
 function sayHey() {
     console.log('hey!');
 }
+
+function conversation() {
+    sayHey();
+    console.log('How are you?');
+    console.log('Goodbye');
+}
+sayHey();
+conversation();
